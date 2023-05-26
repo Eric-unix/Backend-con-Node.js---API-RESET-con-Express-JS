6b0f437a07ecbed86a57162b6696c9df73af6f9f
Eric Felipe <erixlopez6@gmail.com>
2023-05-25 21:17:49 -0600

    introduccion a servicios crea tu primer servicio

---
+++ services/product.service.js
@@ -0,0 +1,45 @@
+const faker = require('faker');
+
+class ProductService {
+
+  constructor(){
+    this.products = [];
+    this.generate();
+  }
+
+  generate(){
+    const limit = 100;
+    for(let index = 0; index <limit; index ++){
+      this.products.push({
+      name:faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl()
+    });
+  }
+  }
+
+  create(){
+
+  }
+
+  find(){
+    return this.products;
+
+  }
+
+  findOne(){
+
+  }
+
+  update(){
+
+  }
+
+  delete(){
+
+  }
+
+
+}
+
+module.exports = ProductService;
