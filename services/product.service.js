3670e471a132c60dc379356f2bed5eef6a04365a
Eric Felipe <erixlopez6@gmail.com>
2023-05-30 11:11:53 -0600

    async await y captura de errores '

--- services/product.service.js
+++ services/product.service.js
@@ -19,7 +19,7 @@ class ProductService {
   }
   }
 
-  create(data){
+  async create(data){
     const newProduct = {
       id: faker.datatype.uuid(),
       ...data
@@ -29,16 +29,19 @@ class ProductService {
   }
 
   find(){
-    return this.products;
-
+    return new Promise((resolve, reject) =>{
+      setTimeout(() =>{
+        resolve(this.products);
+      },5000);
+    })
   }
 
-  findOne(id){
+  async findOne(id){
     return this.products.find(item =>item.id === id);
 
   }
 
-  update(id, changes){
+  async update(id, changes){
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){
       throw new Error('Product not found');
@@ -52,7 +55,7 @@ class ProductService {
 
   }
 
-  delete(id){
+  async delete(id){
     const index = this.products.findIndex(item => item.id === id);
     if(index === -1){
       throw new Error('Product not found');
