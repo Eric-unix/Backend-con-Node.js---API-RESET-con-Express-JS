448596fae18eb712375a7ec477877e1d74499c88
Eric Felipe <erixlopez6@gmail.com>
2023-06-04 02:17:34 -0600

    deployment a vercel

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const routerApi = require('./routes');
 
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -19,14 +19,14 @@ const options = {
   }
 }
 
-app.use(cors());
+app.use(cors(options));
 
-app.get('/', (req,res) =>{
+app.get('/api/', (req,res) =>{
   res.send('Hola mi server en express');
 });
 
 
-app.get('/nueva-ruta', (req,res) =>{
+app.get('a/pi/nueva-ruta', (req,res) =>{
   res.send('Hola soy una nueva ruta');
 });
 
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const faker = require('faker');
+
 
 
 const productsRouter = require('./products.router');
